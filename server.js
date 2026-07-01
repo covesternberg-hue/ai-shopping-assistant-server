5163f1d2c77db64b29c174317a0e392637c7a2d7
covesternberg-hue <covesternberg@gmail.com>
2026-06-30 19:15:56 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
+import express from "express";
 
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("AI Shopping Assistant Server is running!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
